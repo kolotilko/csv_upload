7746499211a00cc3450992718a8d7a312c085ce3
Simon <smreshetnyak@gmail.com>
2016-08-15 18:51:53 +0300

    Попытка использовать библиотеку csv

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ import random
 import pandas as pd
 from urllib.request import urlopen
 from celery_run import celery_app
+import csv
 
 @celery_app.task(bind=True)
 def upload_from_disk(self, file_path):
@@ -21,12 +22,14 @@ def upload_from_link(self, link):
     remote_file = urlopen(link)
     # for line in remote_file:
     #     print(line)
-    file = pd.read_csv(remote_file, sep='\t', header=0, encoding='utf-8')
-    total = random.randint(10, 50)
-    for i, line in enumerate(file.iterrows()):
-        self.update_state(state='PROGRESS',
-                          meta={'current': i, 'total': file.shape[0],
-                                'status': 'Processing'})
+    file = pd.read_csv(remote_file, sep='\t', header=0, encoding='utf-8', chunksize=5)
+
+    for line in file:
+        print(line)
+    # for i, line in enumerate(file.iterrows()):
+    #     self.update_state(state='PROGRESS',
+    #                       meta={'current': i, 'total': file.shape[0],
+    #                             'status': 'Processing'})
 
     return {'current': 100, 'total': 100, 'status': 'Task completed!',
             'result': 42}
