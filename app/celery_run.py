904f8609fbd2911f86ec1fb52a2b04825286921a
Simon <smreshetnyak@gmail.com>
2016-08-17 19:05:29 +0300

    README-файл

--- app/celery_run.py
+++ app/celery_run.py
@@ -5,9 +5,12 @@
 from __future__ import absolute_import
 from celery import Celery
 
+
+CELERY_BROKER='amqp://guest@localhost//'
+CELERY_BACKEND='redis://localhost'
 celery_app = Celery('app.celery_run',
-                    broker='amqp://guest@localhost//',
-                    backend='redis://localhost',
+                    broker=CELERY_BROKER,
+                    backend=CELERY_BACKEND,
                     include=['app.tasks'])
 
 celery_app.conf.update(
